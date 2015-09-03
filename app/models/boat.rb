183729d8eb03d67a8e551785c630b0e25af87ef6
zonika <zchodosh@conncoll.edu>
2015-09-03 17:57:09 -0400

    done

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,36 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    all.limit(5)
+  end
+
+  def self.dinghy
+    all.where('length < 20')
+  end
+
+  def self.ship
+    all.where('length > 20')
+  end
+
+  def self.last_three_alphabetically
+    all.order("name DESC").limit(3)
+  end
+
+  def self.without_a_captain
+    all.where(captain_id: nil)
+  end
+
+  def self.sailboats
+    i = BoatClassification.where(classification_id:2).pluck(:boat_id)
+    all.where(id: i)
+  end
+
+  def self.with_three_classifications
+    i = BoatClassification.all.pluck(:boat_id)
+    s = i.select{|item| i.count(item) == 3}.uniq
+    all.where(id: s)
+  end
+
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,37 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    b = BoatClassification.where(classification_id:3).pluck(:boat_id)
+    c = Boat.where(id: b).pluck(:captain_id)
+    all.where(id:c)
+  end
+
+  def self.sailors
+    s = Boat.sailboats.pluck(:captain_id)
+    all.where(id:s)
+  end
+
+  def self.talented_seamen
+    motorboats = BoatClassification.where(classification_id:5).pluck(:boat_id)
+    sailboats = Boat.sailboats.pluck(:id)
+    ids = all.select do |captain|
+      boats = captain.boats.pluck(:id)
+      if boats & motorboats != [] && boats & sailboats != []
+        captain
+      end
+    end
+    ids.collect do |cap|
+      cap.id
+    end
+    all.where(id:ids)
+  end
+
+  def self.non_sailors
+    ns = all - sailors
+    ns.collect do |cap|
+      cap.id
+    end
+    all.where(id:ns)
+  end
 end
